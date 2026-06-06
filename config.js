184bb287b0cc5802d7d36b06ba5af771ddf49937
lianghui1988l <115056252@qq.com>
2026-06-06 15:00:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  siteName: '龚仔的摄影日记',
+  siteName: 'Aris的摄影日记',
   siteTitle: '记录生活，定格美好',
   siteDescription: '用镜头捕捉每一个值得珍藏的瞬间',
   footer: {
     social: [
-      { label: '小红书', url: 'https://xhslink.com/m/8MA6NoHQesz', brand: 'xiaohongshu' },
-      { label: '抖音', url: 'https://v.douyin.com/owyNGXrRXrA/', brand: 'douyin' },
-      { label: '微信：GongzVirAug' },
-      { label: '邮箱：jogt@foxmail.com', url: 'mailto:jogt@foxmail.com' }
+      { label: '小红书', url: 'https://xhslink.com/m/62dhqid4Y2C', brand: 'xiaohongshu' },
+      { label: '抖音', url: 'https://v.douyin.com/1vy4Hzv4zHU/', brand: 'douyin' },
+      { label: '微信：lianghui1988l' },
+      { label: '邮箱：115056252@qq.com', url: 'mailto:115056252@qq.com' }
     ]
   }
 };
